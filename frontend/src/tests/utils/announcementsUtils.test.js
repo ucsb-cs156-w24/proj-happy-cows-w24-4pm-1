7b3f34057702169cad905578ca3866d47e1ca3c8
Alexander <alexanderthomas@ucsb.edu>
2024-03-05 15:45:25 -0800

    added utils tests

---
+++ frontend/src/tests/utils/announcementsUtils.test.js
@@ -0,0 +1,51 @@
+import { cellToAxiosParamsDelete, commonsNotJoined, onDeleteSuccess } from "main/utils/announcementsUtils";
+import mockConsole from "jest-mock-console";
+import announcementsFixtures from "fixtures/announcementsFixtures";
+
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
+
+describe("AnnouncementssUtils", () => {
+    describe("onDeleteSuccess", () => {
+        test("It puts the message on console.log and in a toast", () => {
+            // arrange
+            const restoreConsole = mockConsole();
+
+            // act
+            onDeleteSuccess("abc");
+            
+            // assert
+            expect(mockToast).toHaveBeenCalledWith("abc");
+            expect(console.log).toHaveBeenCalled();
+            const message = console.log.mock.calls[0][0];
+            expect(message).toMatch("abc");
+
+            restoreConsole();
+        });
+
+    });
+    describe("cellToAxiosParamsDelete", () => {
+        test("It returns the correct params", () => {
+            // arrange
+            const cell = { row: { values: { "announcements.id" : 17 } } };
+
+            // act
+            const result = cellToAxiosParamsDelete(cell);
+
+            // assert
+            expect(result).toEqual({
+                url: "/api/announcements",
+                method: "DELETE",
+                params: { id: 17 }
+            });
+        });
+    });
+
+});
