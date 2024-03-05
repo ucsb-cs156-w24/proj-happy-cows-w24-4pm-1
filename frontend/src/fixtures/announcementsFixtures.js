e65906f99b7d74c73c3453245ed9f3204bc5ed5e
Likhit Gudisay <l_gudisay@ucsb.edu>
2024-03-04 17:28:50 -0800

    dfa

--- frontend/src/fixtures/announcementsFixtures.js
+++ frontend/src/fixtures/announcementsFixtures.js
@@ -2,26 +2,26 @@ const announcementsFixtures = {
     oneAnnouncement: {
         "id": 1,
         "commonsId": 1,
-        "start": "2012-03-05T15:50",
-        "end": "2012-03-05T16:00",
+        "start": "2012-03-05T15:50:00",
+        "end": "2012-03-05T16:00:00",
         "announcement": "single Announcement test" 
     },
     commonsAnnouncements:[
         {
             "id": 1,
             "commonsId": 1,
-            "start": "2012-03-05T15:50",
-            "end": "2012-03-05T16:00",
+            "start": "2012-03-05T15:50:00",
+            "end": "2012-03-05T16:00:00",
             "announcement": "common Announcements test 1" 
         },
         {
             "id": 2,
             "commonsId": 1,
-            "start": "2012-03-05T16:30",
-            "end": "2012-03-05T17:00",
+            "start": "2012-03-05T16:30:00",
+            "end": "2012-03-05T17:00:00",
             "announcement": "common Announcements test 2" 
         }
-    ]
-};
+    ],
+}
 
-export default announcementsFixtures;
+export { announcementsFixtures};
