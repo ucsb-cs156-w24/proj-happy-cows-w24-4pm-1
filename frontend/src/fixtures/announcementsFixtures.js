550539e59409c088cbf958681df55990cc026869
Likhit Gudisay <l_gudisay@ucsb.edu>
2024-03-04 17:00:22 -0800

    fixtures

--- frontend/src/fixtures/announcementsFixtures.js
+++ frontend/src/fixtures/announcementsFixtures.js
@@ -1,29 +1,25 @@
 const announcementsFixtures = {
     oneAnnouncement: {
-                "id": 1,
-                "commonsId": 1,
-                "start": "2012-03-05T15:50",
-                "end": "2012-03-05T16:00",
-                "announcement": "single Announcement test" 
+        "id": 1,
+        "commonsId": 1,
+        "start": "2012-03-05T15:50",
+        "end": "2012-03-05T16:00",
+        "announcement": "single Announcement test" 
     },
     commonsAnnouncements:[
         {
-            "announcements": {
-                "id": 1,
-                "commonsId": 1,
-                "start": "2012-03-05T15:50",
-                "end": "2012-03-05T16:00",
-                "announcement": "common Announcements test 1" 
-            }
+            "id": 1,
+            "commonsId": 1,
+            "start": "2012-03-05T15:50",
+            "end": "2012-03-05T16:00",
+            "announcement": "common Announcements test 1" 
         },
         {
-            "announcements": {
-                "id": 2,
-                "commonsId": 1,
-                "start": "2012-03-05T16:30",
-                "end": "2012-03-05T17:00",
-                "announcement": "common Announcements test 2" 
-            }
+            "id": 2,
+            "commonsId": 1,
+            "start": "2012-03-05T16:30",
+            "end": "2012-03-05T17:00",
+            "announcement": "common Announcements test 2" 
         }
     ]
 }
